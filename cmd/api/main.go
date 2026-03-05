ba09a1042671453b07c9ff8fd082067f16225590
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-05 00:22:32 -0300

    feat: comentários main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"github.com/BrunoPessoa097/api-go-simples/internal/handlers"
 	"github.com/BrunoPessoa097/api-go-simples/internal/route"
 	"github.com/gin-gonic/gin"
 )
 
+// função principal
 func main() {
 	// desativando release mode
 	gin.SetMode(gin.ReleaseMode)
 	// iniciando servidor
 	r := gin.Default()
+
+	// handles inicial
+	handleDefault := handlers.NewDefaultHandle()
+	// rotas padrão
+	route := route.NewRotasDefault(handleDefault)
 
 	// routeamento
 	route.InicialRota(r)
