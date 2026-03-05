ea1fb048b2e316e675f8aec1ec5364d39372d34d
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-05 00:23:04 -0300

    feat: comentários do handle principal

--- internal/handlers/principal_handle.go
+++ internal/handlers/principal_handle.go
@@ -2,28 +2,33 @@ package handlers
 
 import (
 	"net/http"
-	"time"
 
+	"github.com/BrunoPessoa097/api-go-simples/internal/utils"
 	"github.com/gin-gonic/gin"
 )
 
-// data so servidor
-var dtHr = time.Now().Format("01-02-2006 15:04:05")
+// estrutura principal
+type DefaultHandle struct{}
+
+// construtor
+func NewDefaultHandle() *DefaultHandle {
+	return &DefaultHandle{}
+}
 
 // saida da rota principal
-func InicialHandle(c *gin.Context) {
+func (d *DefaultHandle) InicialHandle(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"mensagem":  "API simples em golang(estudos 3/3)",
 		"versao":    "v0.0.0",
 		"autor":     "Bruno Pessoa",
-		"data/hora": dtHr,
+		"data/hora": utils.DtHr,
 	})
 }
 
 // saida da rota não encontrada
-func NaoEncontrada(c *gin.Context) {
+func (d *DefaultHandle) NaoEncontrada(c *gin.Context) {
 	c.JSON(http.StatusNotFound, gin.H{
 		"menssage":  "rota não encontrada",
-		"data/hora": dtHr,
+		"data/hora": utils.DtHr,
 	})
 }
