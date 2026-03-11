5fcad989f49b34417b77627f77585d45c9e33d44
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-11 04:53:21 -0300

    feat: adicionando comentarios do handle princial

--- internal/handlers/principal_handle_test.go
+++ internal/handlers/principal_handle_test.go
@@ -9,34 +9,44 @@ import (
 	"github.com/go-openapi/testify/v2/assert"
 )
 
+// tdd inicial handle
 func TestInitialHandle(t *testing.T) {
+	// iniciando
 	gin.SetMode(gin.TestMode)
 
 	router := gin.Default()
 	handler := NewDefaultHandle()
 
+	// rotas
 	router.GET("/", handler.InicialHandle)
 
+	// entradas
 	req := httptest.NewRequest(http.MethodGet, "/", nil)
 	w := httptest.NewRecorder()
 
 	router.ServeHTTP(w, req)
 
+	// saida
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.NotEqual(t, http.StatusNotFound, "Problemas com a rota principal")
 }
 
+// tdd não encontrado
 func TestNaoEncontrado(t *testing.T) {
+	// iniciando
 	handler := NewDefaultHandle()
 
+	// rotas
 	router := gin.Default()
-
 	router.NoRoute(handler.NaoEncontrada)
 
+	// definicao das entradas
 	req, _ := http.NewRequest(http.MethodGet, "/error", nil)
 	w := httptest.NewRecorder()
 
+	// entradas e saidas
 	router.ServeHTTP(w, req)
 
+	// saida
 	assert.Equal(t, http.StatusNotFound, w.Code)
 }
