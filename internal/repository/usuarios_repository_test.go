6517704bc6b80d5279bef22e79d4de4550ca7469
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-13 03:18:36 -0300

    test: adicionando teste de adicionar

--- internal/repository/usuarios_repository_test.go
+++ internal/repository/usuarios_repository_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/BrunoPessoa097/api-go-simples/internal/mocks"
+	"github.com/BrunoPessoa097/api-go-simples/internal/models"
 	"github.com/go-openapi/testify/v2/assert"
 )
 
@@ -20,4 +21,25 @@ func TestUsuarioRepositoryList(t *testing.T) {
 		assert.Equal(t, saida, esp)
 	}
 
+}
+
+// adicionar
+func TestUsuarioRepositoryAdd(t *testing.T) {
+	//base
+	espec := models.UsuarioCriate{
+		Id:        0,
+		Nome:      "Bruno",
+		Email:     "brunopessoa@gmail.com",
+		Senha:     "1234",
+		Role:      1,
+		Bloqueado: false,
+	}
+
+	// iniciando
+	repo := NewUsuarioRepository()
+
+	//saida
+	if saida := repo.UsuarioRepositoryAdd(espec); saida {
+		assert.Equal(t, saida, true)
+	}
 }
