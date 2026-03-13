d39da805489c67e2daae842cf5a0267a228c5017
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-13 05:21:46 -0300

    refactor: melhoria nos services

--- internal/services/usuario_services.go
+++ internal/services/usuario_services.go
@@ -1,42 +1,42 @@
 package services
 
-import "github.com/BrunoPessoa097/api-go-simples/internal/models"
+import (
+	"github.com/BrunoPessoa097/api-go-simples/internal/models"
+	"github.com/BrunoPessoa097/api-go-simples/internal/repository"
+)
 
-type UsuarioService struct{}
+type UsuarioService struct {
+	repo *repository.UsuarioRepository
+}
 
 // construtor
-func NewUsuarioService() *UsuarioService {
-	return &UsuarioService{}
+func NewUsuarioService(repo *repository.UsuarioRepository) *UsuarioService {
+	return &UsuarioService{
+		repo: repo,
+	}
 }
 
 // listar
 func (s *UsuarioService) UsuarioServiceList() []models.UsuarioCriate {
-	return make([]models.UsuarioCriate, 0)
+	return s.repo.UsuarioRepositoryList()
 }
 
 // adiciona
-func (s *UsuarioService) UsuarioServiceAdd() bool {
-	return true
+func (s *UsuarioService) UsuarioServiceAdd(user models.UsuarioCriate) bool {
+	return s.repo.UsuarioRepositoryAdd(user)
 }
 
 // buscar por id
-func (s *UsuarioService) UsuarioServiceById(id int) models.UsuarioCriate {
-	return models.UsuarioCriate{
-		Id:        0,
-		Nome:      "Bruno",
-		Email:     "brunopessoa@gmail.com",
-		Senha:     "1234",
-		Role:      1,
-		Bloqueado: false,
-	}
+func (s *UsuarioService) UsuarioServiceById(id int64) *models.UsuarioCriate {
+	return s.repo.UsuarioRepositoryById(int32(id))
 }
 
 // update
 func (s *UsuarioService) UsuarioServiceUpdate(id int, user models.UsuarioCriate) bool {
-	return true
+	return s.repo.UsuarioRepositoryUpdate(int32(id), user)
 }
 
 // delete
 func (s *UsuarioService) UsuarioServiceDelete(id int) bool {
-	return true
+	return s.repo.UsuarioRepositoryDelete(int32(id))
 }
