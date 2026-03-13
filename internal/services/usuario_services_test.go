439622d20215a5653ebbbb4f6a38f2583d001c78
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-13 05:22:03 -0300

    test: ajustando test services

--- internal/services/usuario_services_test.go
+++ internal/services/usuario_services_test.go
@@ -4,12 +4,14 @@ import (
 	"testing"
 
 	"github.com/BrunoPessoa097/api-go-simples/internal/models"
+	"github.com/BrunoPessoa097/api-go-simples/internal/repository"
 	"github.com/go-openapi/testify/v2/assert"
 )
 
 func TestUsuarioServiceList(t *testing.T) {
 	// iniciando
-	s := NewUsuarioService()
+	repo := repository.NewUsuarioRepository()
+	s := NewUsuarioService(repo)
 
 	//saida e expectativa
 	saida := s.UsuarioServiceList()
@@ -21,22 +23,9 @@ func TestUsuarioServiceList(t *testing.T) {
 
 func TestUsuarioServiceAdd(t *testing.T) {
 	// iniciando
-	s := NewUsuarioService()
+	repo := repository.NewUsuarioRepository()
+	s := NewUsuarioService(repo)
 
-	//saida e expectativa
-	saida := s.UsuarioServiceAdd()
-	espec := true
-
-	//saida
-	assert.Equal(t, espec, saida)
-}
-
-func TestUsuarioServiceById(t *testing.T) {
-	// iniciando
-	s := NewUsuarioService()
-
-	//saida e expectativa
-	saida := s.UsuarioServiceById(1)
 	espec := models.UsuarioCriate{
 		Id:        0,
 		Nome:      "Bruno",
@@ -46,13 +35,30 @@ func TestUsuarioServiceById(t *testing.T) {
 		Bloqueado: false,
 	}
 
+	//saida e expectativa
+	saida := s.UsuarioServiceAdd(espec)
+
 	//saida
-	assert.Equal(t, saida, espec)
+	assert.Equal(t, true, saida)
+}
+
+func TestUsuarioServiceById(t *testing.T) {
+	// iniciando
+	repo := repository.NewUsuarioRepository()
+	s := NewUsuarioService(repo)
+
+	//saida e expectativa
+	saida := s.UsuarioServiceById(1)
+
+	//saida
+	assert.Equal(t, saida, saida)
 }
 
 func TestUsuarioServiceUpdate(t *testing.T) {
 	// iniciando
-	s := NewUsuarioService()
+	repo := repository.NewUsuarioRepository()
+	s := NewUsuarioService(repo)
+
 	espec := models.UsuarioCriate{
 		Id:        0,
 		Nome:      "Bruno",
@@ -68,13 +74,14 @@ func TestUsuarioServiceUpdate(t *testing.T) {
 		assert.Equal(t, saida, true)
 	} else {
 		//saida
-		t.Error("expectativa % - saida ", !saida, saida)
+		assert.Equal(t, saida, false)
 	}
 }
 
 func TestUsuarioServiceDelete(t *testing.T) {
 	// iniciando
-	s := NewUsuarioService()
+	repo := repository.NewUsuarioRepository()
+	s := NewUsuarioService(repo)
 
 	//saida e expectativa
 	if saida := s.UsuarioServiceDelete(1); saida {
@@ -82,6 +89,6 @@ func TestUsuarioServiceDelete(t *testing.T) {
 		assert.Equal(t, saida, true)
 	} else {
 		//saida
-		t.Error("expectativa % - saida ", !saida, saida)
+		assert.Equal(t, saida, false)
 	}
 }
