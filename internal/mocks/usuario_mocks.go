3cedb04e1a2a203e4f206f884db4c139a39fd80b
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-14 02:52:45 -0300

    feat: adicionando mocks

--- internal/mocks/usuario_mocks.go
+++ internal/mocks/usuario_mocks.go
@@ -3,5 +3,22 @@ package mocks
 import "github.com/BrunoPessoa097/api-go-simples/internal/models"
 
 var (
-	UsuariosBD = make([]models.UsuarioCriate, 0)
+	UsuariosBD = []models.UsuarioCriate{
+		{
+			Id:        1,
+			Nome:      "Bruno F",
+			Email:     "brunopessoa@gmail.com",
+			Senha:     "1234",
+			Role:      1,
+			Bloqueado: false,
+		},
+		{
+			Id:        2,
+			Nome:      "Bruno P",
+			Email:     "brunopessoa@gmail.com",
+			Senha:     "1234",
+			Role:      1,
+			Bloqueado: false,
+		},
+	}
 )
