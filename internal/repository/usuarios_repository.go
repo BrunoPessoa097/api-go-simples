74e5e4fc0e36aaa2a45523dfa6e6f77d35e59310
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-14 03:39:46 -0300

    refactor: repository update melhoria

--- internal/repository/usuarios_repository.go
+++ internal/repository/usuarios_repository.go
@@ -54,22 +54,22 @@ func (r *UsuarioRepository) UsuarioRepositoryById(id int32) *models.UsuarioCriat
 }
 
 // update
-func (r *UsuarioRepository) UsuarioRepositoryUpdate(id int32, update models.UsuarioCriate) bool {
+func (r *UsuarioRepository) UsuarioRepositoryUpdate(id int32, update models.UsuarioCriate) (error, bool) {
 	//mocks
 	mokerUser := mocks.UsuariosBD
 	userUp := update
 
-	// saida := r.UsuarioRepositorySearch()
+	if saida := r.UsuarioRepositorySearch(userUp.Nome, userUp.Email); saida {
+		return errors.New("Usuario e/ou E-mail já cadastrados"), true
+	}
 
 	// buscando
 	for i, user := range mokerUser {
 		if user.Id == id {
 			mokerUser[i] = userUp
-			return true
 		}
 	}
-
-	return false
+	return nil, false
 }
 
 // usuario delete
