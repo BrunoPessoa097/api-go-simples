3ca93e1b3d051bd194646cd7ec0f6e3780fa31b6
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-15 03:48:56 -0300

    refactor: melhorias no models

--- internal/models/usuario_models.go
+++ internal/models/usuario_models.go
@@ -5,9 +5,9 @@ import "time"
 // model de usuário
 type UsuarioCriate struct {
 	Id        int32  `json:"id"`
-	Nome      string `json:"nome"`
-	Email     string `json:"email"`
-	Senha     string `json:"senha"`
+	Nome      string `json:"nome" binding:"required,min=3,max=20"`
+	Email     string `json:"email" binding:"required,email"`
+	Senha     string `json:"senha" binding:"required,min=8,max=15"`
 	Role      int64  `json:"role"`
 	Bloqueado bool   `json:"bloqueado"`
 	DtCreate  time.Time
