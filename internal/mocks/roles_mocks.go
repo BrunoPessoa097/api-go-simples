f31b374866573e1598729985b58eec65369c09fd
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-18 20:15:02 -0300

    feat: mocks de roles

---
+++ internal/mocks/roles_mocks.go
@@ -0,0 +1,21 @@
+package mocks
+
+import "github.com/BrunoPessoa097/api-go-simples/internal/models"
+
+var ListRoles = []models.RolesC{
+	{
+		ID:    1,
+		Nivel: "ADM",
+		Regra: "get,post,delete,put",
+	},
+	{
+		ID:    2,
+		Nivel: "Gerente",
+		Regra: "get,post,delete,put",
+	},
+	{
+		ID:    3,
+		Nivel: "publico",
+		Regra: "get",
+	},
+}
