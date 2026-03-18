b7b5e3e3fa7ffc2cefb0192d4b297efd2a5aa6b4
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-18 18:08:55 -0300

    feat: criando estrutura de regras

---
+++ internal/models/roles_models.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type RolesC struct {
+	ID       int64  `json:"id"`
+	Nivel    string `json:"nivel" binding:"required,min=2,max=10"`
+	Regra    string `json:"regra" binding:"required,min=2,max=10"`
+	DtCreate time.Time
+	DtUpdate time.Time
+}
