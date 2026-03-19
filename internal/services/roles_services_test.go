37a242e67ef0e35a7a7257326b3abe375b74fe3e
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-18 23:39:24 -0300

    test(roles): adicionando buscar ou post

--- internal/services/roles_services_test.go
+++ internal/services/roles_services_test.go
@@ -3,6 +3,7 @@ package services
 import (
 	"testing"
 
+	"github.com/BrunoPessoa097/api-go-simples/internal/models"
 	"github.com/go-openapi/testify/v2/assert"
 )
 
@@ -16,4 +17,28 @@ func TestRoleServiceList(t *testing.T) {
 
 	//verificação
 	assert.Equal(t, "ADM", saida[0].Nivel)
+}
+
+func TestRoleServiceSearch(t *testing.T) {
+	//iniciando
+	s := NewRoleService()
+
+	// buscando algo
+	saida := s.RoleServiceSearch("ADM")
+
+	//saida
+	assert.Equal(t, true, saida)
+}
+
+func TestRoleServicePost(t *testing.T) {
+	s := NewRoleService()
+
+	role := models.RolesC{
+		Nivel: "Governador",
+		Regra: "ler",
+	}
+
+	saida, _ := s.RoleServicePost(role)
+
+	assert.Equal(t, true, saida)
 }
