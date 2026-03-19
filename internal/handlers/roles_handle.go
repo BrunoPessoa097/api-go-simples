dc2b13ff0329768e3c65485a7eab51db368931b6
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-18 21:07:40 -0300

    feat(handle): adicionando buscar por regras

--- internal/handlers/roles_handle.go
+++ internal/handlers/roles_handle.go
@@ -6,20 +6,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// estrutura
 type RolesHandler struct{}
 
+// construtor
 func NewRolesHandler() *RolesHandler {
 	return &RolesHandler{}
 }
 
+// list
 func (r *RolesHandler) RolesHandlerList(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"mensage": "Listar regras",
 	})
 }
 
+// post
 func (r *RolesHandler) RolesHandlerPost(c *gin.Context) {
 	c.JSON(http.StatusCreated, gin.H{
 		"mensage": "adicionar regras",
+	})
+}
+
+// buscar por id
+func (r *RolesHandler) RolesHandlerById(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "buscar por id",
 	})
 }
