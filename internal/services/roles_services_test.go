ed89e247810e676fc9e81eff88710ebfaac12e70
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-19 20:48:04 -0300

    test(roles):buscar por id

--- internal/services/roles_services_test.go
+++ internal/services/roles_services_test.go
@@ -19,6 +19,7 @@ func TestRoleServiceList(t *testing.T) {
 	assert.Equal(t, "ADM", saida[0].Nivel)
 }
 
+// busca
 func TestRoleServiceSearch(t *testing.T) {
 	//iniciando
 	s := NewRoleService()
@@ -30,15 +31,33 @@ func TestRoleServiceSearch(t *testing.T) {
 	assert.Equal(t, true, saida)
 }
 
+// adicao
 func TestRoleServicePost(t *testing.T) {
+	//iniciar
 	s := NewRoleService()
 
+	//modelo
 	role := models.RolesC{
 		Nivel: "Governador",
 		Regra: "ler",
 	}
 
+	//saida
 	saida, _ := s.RoleServicePost(role)
 
+	// saida
 	assert.Equal(t, true, saida)
+}
+
+// busca por id
+func TestRoleServiceById(t *testing.T) {
+	//iniciando
+	s := NewRoleService()
+
+	// expectativa
+	esp := "ADM"
+	saida := s.RoleServiceById(1)
+
+	//saido
+	assert.Equal(t, esp, saida.Nivel)
 }
