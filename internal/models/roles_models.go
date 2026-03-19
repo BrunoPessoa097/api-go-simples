9cc84cf3c5b7073e4f37c5e5a256d9b8933ece03
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-18 23:35:23 -0300

    refactor(models): ajustando tamanho de niveil de roles

--- internal/models/roles_models.go
+++ internal/models/roles_models.go
@@ -5,7 +5,7 @@ import "time"
 type RolesC struct {
 	ID       int64  `json:"id"`
 	Nivel    string `json:"nivel" binding:"required,min=2,max=10"`
-	Regra    string `json:"regra" binding:"required,min=2,max=10"`
+	Regra    string `json:"regra" binding:"required,min=2,max=50"`
 	DtCreate time.Time
 	DtUpdate time.Time
 }
