6b658de00c0d9dd6e84898f042a27d787d8fe4cb
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-22 00:30:24 -0300

    test(melhoria): melhoria nos testes

--- internal/handlers/usuario_handle_test.go
+++ internal/handlers/usuario_handle_test.go
@@ -1,4 +1,4 @@
-package handlers
+package handlers_test
 
 import (
 	"bytes"
@@ -8,8 +8,7 @@ import (
 	"testing"
 
 	"github.com/BrunoPessoa097/api-go-simples/internal/models"
-	"github.com/BrunoPessoa097/api-go-simples/internal/repository"
-	"github.com/BrunoPessoa097/api-go-simples/internal/services"
+	"github.com/BrunoPessoa097/api-go-simples/internal/start"
 	"github.com/gin-gonic/gin"
 	"github.com/go-openapi/testify/v2/assert"
 )
@@ -19,9 +18,8 @@ func TestUsuarioListHandle(t *testing.T) {
 	// inicializando
 	gin.SetMode(gin.TestMode)
 	router := gin.Default()
-	repo := repository.NewUsuarioRepository()
-	service := services.NewUsuarioService(repo)
-	handle := NewUsuarioHandle(service)
+	s := start.NewStart()
+	handle := s.UsuarioStart()
 
 	// requisição
 	req := httptest.NewRequest(http.MethodGet, "/usuarios", nil)
@@ -42,9 +40,8 @@ func TestUsuarioPostHandle(t *testing.T) {
 	// iniciando
 	gin.SetMode(gin.TestMode)
 	router := gin.Default()
-	repo := repository.NewUsuarioRepository()
-	service := services.NewUsuarioService(repo)
-	handle := NewUsuarioHandle(service)
+	s := start.NewStart()
+	handle := s.UsuarioStart()
 
 	// entrada
 	msg := models.UsuarioCriate{
@@ -81,9 +78,8 @@ func TestUsuarioByIdHandle(t *testing.T) {
 	//iniciar
 	gin.SetMode(gin.TestMode)
 	router := gin.Default()
-	repo := repository.NewUsuarioRepository()
-	service := services.NewUsuarioService(repo)
-	handler := NewUsuarioHandle(service)
+	s := start.NewStart()
+	handler := s.UsuarioStart()
 
 	//requesicao
 	req := httptest.NewRequest(http.MethodGet, "/usuarios/:id", nil)
@@ -108,9 +104,8 @@ func TestUsuarioUpdateHandle(t *testing.T) {
 	//inicializando
 	gin.SetMode(gin.TestMode)
 	route := gin.Default()
-	repo := repository.NewUsuarioRepository()
-	service := services.NewUsuarioService(repo)
-	handler := NewUsuarioHandle(service)
+	s := start.NewStart()
+	handler := s.UsuarioStart()
 
 	// entrada
 	msg := models.UsuarioCriate{
@@ -121,10 +116,11 @@ func TestUsuarioUpdateHandle(t *testing.T) {
 		Bloqueado: false,
 	}
 
+	//convertendo para json
 	user, _ := json.Marshal(&msg)
 
 	//requisicao
-	req := httptest.NewRequest(http.MethodPut, "/usuarios/:id", bytes.NewBuffer(user))
+	req := httptest.NewRequest(http.MethodPut, "/usuarios/1", bytes.NewBuffer(user))
 	req.Header.Set("Content-Type", "application/json")
 	w := httptest.NewRecorder()
 
@@ -132,13 +128,8 @@ func TestUsuarioUpdateHandle(t *testing.T) {
 	route.PUT("/usuarios/:id", handler.UsuarioUpdateHandle)
 	route.ServeHTTP(w, req)
 
-	// convertendo bites em json
-	var resp models.UsuarioCriate
-	json.Unmarshal(w.Body.Bytes(), &resp)
-
 	//saida
 	assert.Equal(t, 200, w.Code)
-	assert.Contains(t, w.Body.String(), resp.Nome)
 }
 
 // tdd deletar usuarios
@@ -146,12 +137,11 @@ func TestUsuarioDeleteHandle(t *testing.T) {
 	// iniciando
 	gin.SetMode(gin.TestMode)
 	route := gin.Default()
-	repo := repository.NewUsuarioRepository()
-	service := services.NewUsuarioService(repo)
-	handle := NewUsuarioHandle(service)
+	s := start.NewStart()
+	handle := s.UsuarioStart()
 
 	//requisicao
-	req := httptest.NewRequest(http.MethodDelete, "/usuarios/:id", nil)
+	req := httptest.NewRequest(http.MethodDelete, "/usuarios/:1", nil)
 	req.Header.Set("Content-Type", "application/json")
 	w := httptest.NewRecorder()
 
@@ -160,6 +150,5 @@ func TestUsuarioDeleteHandle(t *testing.T) {
 	route.ServeHTTP(w, req)
 
 	//saida
-	assert.Equal(t, http.StatusBadRequest, w.Code)
-
+	assert.Equal(t, http.StatusNoContent, w.Code)
 }
