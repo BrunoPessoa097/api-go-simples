3f01ff692fedd2728de26cdc8b6a8ba0869b160b
Bruno Pessoa <brunopessoa097@gmail.com>
2026-03-23 02:27:50 -0300

    test(roles):adicionando services

--- internal/services/roles_services_test.go
+++ internal/services/roles_services_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/BrunoPessoa097/api-go-simples/internal/mocks"
+	"github.com/BrunoPessoa097/api-go-simples/internal/models"
 	"github.com/BrunoPessoa097/api-go-simples/internal/repository"
 	"github.com/go-openapi/testify/v2/assert"
 )
@@ -22,58 +23,86 @@ func TestRoleServiceList(t *testing.T) {
 	assert.Equal(t, "ADM", saida[0].Nivel)
 }
 
-// // busca
-// func TestRoleServiceSearch(t *testing.T) {
-// 	//iniciando
-// 	s := NewRoleService()
+// busca
+func TestRoleServiceSearch(t *testing.T) {
+	//iniciando
+	mo := mocks.ListRoles
+	rp := repository.NewRolesRepository(mo)
+	s := NewRoleService(rp)
 
-// 	// buscando algo
-// 	saida := s.RoleServiceSearch("ADM")
+	// buscando algo
+	saida := s.RoleServiceSearch("ADM")
 
-// 	//saida
-// 	assert.Equal(t, true, saida)
-// }
+	//saida
+	assert.Equal(t, "Nivel já cadastrado", saida.Error())
+}
 
-// // adicao
-// func TestRoleServicePost(t *testing.T) {
-// 	//iniciar
-// 	s := NewRoleService()
+// adicao
+func TestRoleServicePost(t *testing.T) {
+	//iniciar
+	mocks := mocks.ListRoles
+	repo := repository.NewRolesRepository(mocks)
+	s := NewRoleService(repo)
 
-// 	//modelo
-// 	role := models.RolesC{
-// 		Nivel: "Governador",
-// 		Regra: "ler",
-// 	}
+	//modelo
+	role := models.RolesC{
+		Nivel: "Governador",
+		Regra: "ler",
+	}
 
-// 	//saida
-// 	saida, _ := s.RoleServicePost(role)
+	//saida
+	saida := s.RoleServicePost(role)
 
-// 	// saida
-// 	assert.Equal(t, true, saida)
-// }
+	// saida
+	assert.Equal(t, true, saida)
+}
 
-// // busca por id
-// func TestRoleServiceById(t *testing.T) {
-// 	//iniciando
-// 	s := NewRoleService()
+// busca por id
+func TestRoleServiceById(t *testing.T) {
+	//iniciando
+	mocks := mocks.ListRoles
+	repo := repository.NewRolesRepository(mocks)
+	s := NewRoleService(repo)
 
-// 	// expectativa
-// 	esp := "ADM"
-// 	saida, _ := s.RoleServiceById(1)
+	// expectativa
+	esp := "ADM"
+	saida := s.RoleServiceById(1)
 
-// 	//saido
-// 	assert.Equal(t, esp, saida.Nivel)
-// }
+	//saido
+	assert.Equal(t, esp, saida.Nivel)
+}
 
-// // delete
-// func TestRoleServiceDelete(t *testing.T) {
-// 	// inicializando
-// 	s := NewRoleService()
+// update
+func TestRoleServiceUpdate(t *testing.T) {
+	//iniciar
+	mocks := mocks.ListRoles
+	repo := repository.NewRolesRepository(mocks)
+	s := NewRoleService(repo)
 
-// 	//queries
-// 	esp := true
-// 	saida := s.RoleServiceDelete(1)
+	//modelo
+	role := models.RolesC{
+		ID:    1,
+		Nivel: "Gov",
+		Regra: "ler",
+	}
 
-// 	//saida
-// 	assert.Equal(t, esp, saida)
-// }
+	//saida
+	saida := s.RoleServiceUpdate(1, role)
+
+	// saida
+	assert.Equal(t, nil, saida)
+}
+
+// delete
+func TestRoleServiceDelete(t *testing.T) {
+	// inicializando
+	mocks := mocks.ListRoles
+	repo := repository.NewRolesRepository(mocks)
+	s := NewRoleService(repo)
+
+	//queries
+	saida := s.RoleServiceDelete(1)
+
+	//saida
+	assert.Equal(t, nil, saida)
+}
