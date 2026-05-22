3afe4d18ce52c614c779b814a6e58e76f887a252
BrunoPessoa097 <brunopessoa09@gmail.com>
2026-05-22 02:06:43 -0300

    feat: adiciconando dotenv

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -1,6 +1,23 @@
 package utils
 
-import "time"
+import (
+	"errors"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
 
 // data so servidor
 var DtHr = time.Now().Format("01-02-2006 15:04:05")
+
+// carregar o dotenv
+func Dotenv(variaveis string) (string, error) {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		return "", errors.New("Erro ao carregar as variáveis do .env")
+	}
+
+	return os.Getenv(variaveis), nil
+}
