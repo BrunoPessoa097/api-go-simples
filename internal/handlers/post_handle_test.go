a87aa885dde1714bda1e20834391f739741e0143
BrunoPessoa097 <brunopessoa09@gmail.com>
2026-05-23 02:14:30 -0300

    test(post): adicionando test

---
+++ internal/handlers/post_handle_test.go
@@ -0,0 +1,90 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-openapi/testify/v2/assert"
+)
+
+// teste listagem
+func TestPostHandlerList(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+	p := NewPostHandlers()
+
+	req := httptest.NewRequest(http.MethodGet, "/posts", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.GET("/posts", p.PostHandlersList)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+// inserir postagem
+func TestPostHandlerPost(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+	p := NewPostHandlers()
+
+	req := httptest.NewRequest(http.MethodPost, "/posts", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.POST("/posts", p.PostHandlersPost)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+// selecionar postagem
+func TestPostHandlerById(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+	p := NewPostHandlers()
+
+	req := httptest.NewRequest(http.MethodGet, "/posts/1", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.GET("/posts/:id", p.PostHandlersById)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+// atualizar postagem
+func TestPostHandlerUpdate(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+	p := NewPostHandlers()
+
+	req := httptest.NewRequest(http.MethodPatch, "/posts/1", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.PATCH("/posts/:id", p.PostHandlersById)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+// delete de postagem
+func TestPostHandlerDelete(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	r := gin.Default()
+	p := NewPostHandlers()
+
+	req := httptest.NewRequest(http.MethodDelete, "/posts/1", nil)
+	req.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	r.DELETE("/posts/:id", p.PostHandlersById)
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
