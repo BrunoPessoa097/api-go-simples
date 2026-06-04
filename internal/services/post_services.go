0ba88cccec128ade9250c33e0870517d086edd3a
BrunoPessoa097 <brunopessoa09@gmail.com>
2026-06-04 02:47:01 -0300

    feat(service-post): adicionar postagem

--- internal/services/post_services.go
+++ internal/services/post_services.go
@@ -1,6 +1,9 @@
 package services
 
 import (
+	"time"
+
+	"github.com/BrunoPessoa097/api-go-simples/internal/mocks"
 	"github.com/BrunoPessoa097/api-go-simples/internal/models"
 	"github.com/BrunoPessoa097/api-go-simples/internal/repository"
 )
@@ -18,4 +21,18 @@ func NewPostService(r *repository.PostRepository) *PostService {
 // listagem
 func (p *PostService) PostServiceList() []models.Post {
 	return p.Repo.Data
+}
+
+func (p *PostService) PostServiceAdd(post models.Post) bool {
+	tam := len(mocks.ListPost) - 1
+	id := mocks.ListPost[tam].ID + 1
+
+	posts := models.Post{
+		ID:       id,
+		IDUser:   post.IDUser,
+		Texto:    post.Texto,
+		DtCreate: time.Now(),
+		DtUpdate: time.Now(),
+	}
+	return p.Repo.PostRepositoryAdd(posts)
 }
