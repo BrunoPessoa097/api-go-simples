4bf2bc471e2656cf85ef006593a61e1916344998
BrunoPessoa097 <brunopessoa09@gmail.com>
2026-06-04 03:04:21 -0300

    feat(repository-post): adicionando buscar por ID

--- internal/repository/post_repository.go
+++ internal/repository/post_repository.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"fmt"
+
 	"github.com/BrunoPessoa097/api-go-simples/internal/models"
 )
 
@@ -17,7 +19,18 @@ func (p *PostRepository) PostRepositoryList() []models.Post {
 	return p.Data
 }
 
+// adicionar
 func (p *PostRepository) PostRepositoryAdd(post models.Post) bool {
 	p.Data = append(p.Data, post)
 	return true
+}
+
+// selecionar
+func (p *PostRepository) PostRepositoryById(id int64) (*models.Post, error) {
+	for i := range p.Data {
+		if p.Data[i].ID == id {
+			return &p.Data[i], nil
+		}
+	}
+	return nil, fmt.Errorf("post não encontrado")
 }
