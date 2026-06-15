578537d82de8d03650e8b8a45fc1b55bd1eb71fd
BrunoPessoa097 <brunopessoa09@gmail.com>
2026-06-15 01:41:47 -0300

    refactor(roles-repository): conectando ao banco e fazendo as função pelo gorm

--- internal/repository/roles_repository.go
+++ internal/repository/roles_repository.go
@@ -1,69 +1,70 @@
 package repository
 
 import (
+	"errors"
+
 	"github.com/BrunoPessoa097/api-go-simples/internal/models"
+	"gorm.io/gorm"
 )
 
 type RolesRepository struct {
-	Data []models.Roles
+	Data *gorm.DB
 }
 
-func NewRolesRepository(mr []models.Roles) *RolesRepository {
+func NewRolesRepository(mr *gorm.DB) *RolesRepository {
 	return &RolesRepository{
 		Data: mr,
 	}
 }
 
 // listagem
-func (r *RolesRepository) RolesRepositoryList() []models.Roles {
-	return r.Data
+func (r *RolesRepository) RolesRepositoryList() ([]models.Roles, error) {
+	var roles []models.Roles
+	err := r.Data.Find(&roles).Error
+
+	return roles, err
 }
 
 // buscar
 func (r *RolesRepository) RolesRepositoryBusca(role string) bool {
-	for i := range r.Data {
-		if r.Data[i].Nivel == role {
-			return true
-		}
+	var roles models.Roles
+
+	err := r.Data.
+		Where("nivel = ?", role).
+		First(&roles).
+		Error
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return false
 	}
-	return false
+
+	return err == nil
 }
 
 // adicionar
-func (r *RolesRepository) RolesRepositoryAdd(roles *models.Roles) bool {
-	r.Data = append(r.Data, *roles)
-	return true
+func (r *RolesRepository) RolesRepositoryAdd(roles *models.Roles) error {
+	return r.Data.Create(roles).Error
 }
 
 // buscar id
-func (r *RolesRepository) RolesRepositoryById(id int64) *models.Roles {
-	for i := range r.Data {
-		if r.Data[i].ID == id {
-			return &r.Data[i]
-		}
+func (r *RolesRepository) RolesRepositoryById(id int64) (*models.Roles, error) {
+	var role models.Roles
+
+	err := r.Data.First(&role, id).Error
+
+	if err != nil {
+		return nil, err
 	}
-	return nil
+
+	return &role, nil
 }
 
 // update
-func (r *RolesRepository) RolesRepositoryUpdate(id int64, role models.Roles) bool {
-	for i := range r.Data {
-		if r.Data[i].ID == id {
-			r.Data[i] = role
-			return true
-		}
-	}
-	return false
+func (r *RolesRepository) RolesRepositoryUpdate(role *models.Roles) error {
+	return r.Data.Save(role).Error
 }
 
 // delete
-func (r *RolesRepository) RolesRepositoryDelete(id int64) bool {
-	for i := range r.Data {
-		//caso exista
-		if r.Data[i].ID == id {
-			r.Data = append(r.Data[:i], r.Data[i+1:]...)
-			return true
-		}
-	}
-	return false
+func (r *RolesRepository) RolesRepositoryDelete(id int64) error {
+	return r.Data.Delete(&models.Roles{}, id).Error
 }
