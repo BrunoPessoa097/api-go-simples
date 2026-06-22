95e41d2f86db1ed36186847fcbc618dc455fce1e
BrunoPessoa097 <brunopessoa09@gmail.com>
2026-06-22 10:48:03 -0300

    refactor(usuario-dto): correção no tipo de validação

--- internal/dto/usuario_dto.go
+++ internal/dto/usuario_dto.go
@@ -16,10 +16,10 @@ type UsuarioCriateDTO struct {
 
 // usuario update
 type UsuarioUpdateDTO struct {
-	Nome      *string `json:"nome" binding:"min=3,max=20"`
-	Email     *string `json:"email" binding:"email"`
-	Senha     *string `json:"senha" binding:"min=8,max=15"`
-	Role      *int64  `json:"role"`
+	Nome      *string `json:"nome" validate:"omitempty,min=3,max=20"`
+	Email     *string `json:"email" validate:"omitempty,email"`
+	Senha     *string `json:"senha" validate:"omitempty,min=8,max=15"`
+	Role      *int64  `json:"role" validate:"omitempty,min=8,max=15"`
 	Bloqueado *bool   `json:"bloqueado"`
 }
 
@@ -45,13 +45,27 @@ func ToModel(dto UsuarioCriateDTO) models.Usuario {
 }
 
 func ToUpdate(dto UsuarioUpdateDTO) models.Usuario {
-	return models.Usuario{
-		Nome:      *dto.Nome,
-		Email:     *dto.Email,
-		Senha:     *dto.Senha,
-		Role:      *dto.Role,
-		Bloqueado: *dto.Bloqueado,
+	var user models.Usuario
+
+	if dto.Nome != nil {
+		user.Nome = *dto.Nome
 	}
+	if dto.Email != nil {
+		user.Email = *dto.Email
+	}
+	if dto.Senha != nil {
+		user.Senha = *dto.Senha
+	}
+	if dto.Role != nil {
+		user.Role = *dto.Role
+	}
+	if dto.Bloqueado != nil {
+		user.Bloqueado = *dto.Bloqueado
+	}
+
+	user.DtUpdate = time.Now()
+
+	return user
 }
 
 func ToResponse(u models.Usuario) UsuarioResponseDTO {
