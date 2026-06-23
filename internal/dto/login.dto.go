25cc49f1f9b8a95c2c69cd73ac09bb9c05382365
BrunoPessoa097 <brunopessoa09@gmail.com>
2026-06-23 14:06:49 -0300

    feat(dto-login): converter para modelos

--- internal/dto/login.dto.go
+++ internal/dto/login.dto.go
@@ -1,6 +1,15 @@
 package dto
 
+import "github.com/BrunoPessoa097/api-go-simples/internal/models"
+
 type LoginCreate struct {
-	Email string `json:"email" binding:"required"`
-	Senha string `json:"senha" binding:"required"`
+	Email string `json:"email" binding:"required,email,min=3"`
+	Senha string `json:"senha" binding:"required,min=8"`
+}
+
+func ToLogin(dto *LoginCreate) *models.Login {
+	return &models.Login{
+		Email: dto.Email,
+		Senha: dto.Senha,
+	}
 }
