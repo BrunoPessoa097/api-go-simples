873c448421e0aef02c7697584666a0faa7f9b94a
BrunoPessoa097 <brunopessoa09@gmail.com>
2026-06-25 17:52:08 -0300

    refactor(services-usuario):melhoria no delete

--- internal/services/usuario_services.go
+++ internal/services/usuario_services.go
@@ -8,15 +8,17 @@ import (
 )
 
 type UsuarioService struct {
-	repo  *repository.UsuarioRepository
-	roles *repository.RolesRepository
+	repo     *repository.UsuarioRepository
+	roles    *repository.RolesRepository
+	repoPost *repository.PostRepository
 }
 
 // construtor
-func NewUsuarioService(repo *repository.UsuarioRepository, roles *repository.RolesRepository) *UsuarioService {
+func NewUsuarioService(repo *repository.UsuarioRepository, roles *repository.RolesRepository, post *repository.PostRepository) *UsuarioService {
 	return &UsuarioService{
-		repo:  repo,
-		roles: roles,
+		repo:     repo,
+		roles:    roles,
+		repoPost: post,
 	}
 }
 
@@ -27,16 +29,15 @@ func (s *UsuarioService) UsuarioServiceList() ([]models.Usuario, error) {
 
 // adiciona
 func (s *UsuarioService) UsuarioServiceAdd(user models.Usuario) error {
-	if _, verifi := s.repo.UsuarioRepositorySearch(user.Nome, ""); verifi == true {
+	if _, verifi := s.repo.UsuarioRepositorySearch(user.Nome, "", 0); verifi == true {
 		return errors.New("usuário já cadastrado")
 	}
-	if _, verifi := s.repo.UsuarioRepositorySearch("", user.Email); verifi == true {
+	if _, verifi := s.repo.UsuarioRepositorySearch("", user.Email, 0); verifi == true {
 		return errors.New("e-mail já cadastrado")
 	}
 	if _, err := s.roles.RolesRepositoryById(user.Role); err != nil {
 		return errors.New("regra não encontrada")
 	}
-	// return errors.New("regra não encontrada")
 	return s.repo.UsuarioRepositoryAdd(&user)
 }
 
@@ -48,7 +49,7 @@ func (s *UsuarioService) UsuarioServiceById(id int32) (*models.Usuario, error) {
 // update
 func (s *UsuarioService) UsuarioServiceUpdate(id int32, user models.Usuario) error {
 	user.ID = int32(id)
-	if _, verifi := s.repo.UsuarioRepositorySearch(user.Nome, user.Email); verifi != true {
+	if _, verifi := s.repo.UsuarioRepositorySearch(user.Nome, user.Email, 0); verifi != true {
 		if _, err := s.roles.RolesRepositoryById(int64(user.ID)); err != nil {
 			return s.repo.UsuarioRepositoryUpdate(user)
 		}
@@ -59,5 +60,8 @@ func (s *UsuarioService) UsuarioServiceUpdate(id int32, user models.Usuario) err
 
 // delete
 func (s *UsuarioService) UsuarioServiceDelete(id int32) error {
-	return s.repo.UsuarioRepositoryDelete(id)
+	if err := s.repoPost.PostRepositorySearch(int64(id)); err != nil {
+		return s.repo.UsuarioRepositoryDelete(id)
+	}
+	return errors.New("Há usuário com dependencia")
 }
